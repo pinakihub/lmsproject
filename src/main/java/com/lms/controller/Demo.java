43b0a172302a420e83d81e91136b73360966992f
pinaki <pinakib@trainee.nrifintech.com>
2022-12-21 15:20:24 +0530

    backend linked with frontend

--- src/main/java/com/lms/controller/Demo.java
+++ src/main/java/com/lms/controller/Demo.java
@@ -1,5 +1,16 @@
 package com.lms.controller;
 
-public class Demo {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
+
+public class Demo {
+	@RequestMapping("/home")
+	public String home()
+	{
+		return "home";
+	}
 }
